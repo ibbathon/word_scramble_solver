e89b7849ebb80a03d577543f0e69f2a989b67e71
Ibb Marsh <ibbathon@gmail.com>
2018-07-18 14:31:39 -0400

    Completed

--- solver.py
+++ solver.py
@@ -1,17 +1,34 @@
-#!python3
+#!/usr/bin/env python3
 # Author: Ibb Marsh
 # Created: 2018-07-17
 
 import sys
 
 class WSSolver:
-    def __init__ (self, argv):
-        print("The letters are {}".format(','.join(list(argv[1]))))
+	DICT_FILE_NAME = "dictionary.txt"
 
-    def run (self):
-        pass
+	def __init__ (self, argv):
+		self.letters = sorted(argv[1])
+		self.letterset = set(argv[1])
+
+	def run (self):
+		validwords = []
+		with open(self.DICT_FILE_NAME,'r') as f:
+			for word in f:
+				word = word.strip()
+				wordset = set(word)
+				if wordset <= self.letterset and self.issubword(word):
+					validwords.append(word)
+		validwords.sort(key=len)
+		print(validwords)
+
+	def issubword (self, otherword):
+		for l in self.letterset:
+			if self.letters.count(l) < otherword.count(l):
+				return False
+		return True
 
 
 if __name__ == '__main__':
-    wss = WSSolver(sys.argv)
-    wss.run()
+	wss = WSSolver(sys.argv)
+	wss.run()
